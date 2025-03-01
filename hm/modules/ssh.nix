ccb3da1546d6aeebba57ec5437a4b51235f53e91
Błażej Niewiadomski <blaz.nie@protonmail.com>
2025-03-01 15:45:27 +0000

    git ssh fix

--- hm/modules/ssh.nix
+++ hm/modules/ssh.nix
@@ -4,6 +4,10 @@
     enable = true;
     userKnownHostsFile = "~/.ssh/known_hosts";
     addKeysToAgent = "confirm";
+    extraConfig="
+    Host github.com
+    IdentityFile ~/.ssh/id_ed25519
+    ";
     package = pkgs.openssh;
   };
 
